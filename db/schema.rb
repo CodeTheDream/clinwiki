96fb3ef4dacaf491a7cc637b5d573787acbeec49
Ramiro Rodriguez <ramiro.rodriguez@gmail.com>
2019-11-25 10:31:18 -0500

    add column to store last error when trying to geocode location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_045639) do
+ActiveRecord::Schema.define(version: 2019_11_25_152802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_11_20_045639) do
     t.float "latitude"
     t.float "longitude"
     t.datetime "checked"
+    t.string "last_error"
   end
 
   create_table "reviews", force: :cascade do |t|
